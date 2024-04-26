7798a56afc29c41e75bfe57ed07f2c50a7ade163
Philip Munksgaard <philip@munksgaard.me>
2024-04-26 21:28:04 +0200

    Add nix packaging

--- flake.nix
+++ flake.nix
@@ -5,6 +5,14 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     devshell.url = "github:numtide/devshell";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rambo_bin = {
+      url = "github:munksgaard/rambo/flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -14,6 +22,11 @@
         [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }:
         let
+          craneLib = inputs.crane.lib.${system};
+
+          passwordHashing =
+            craneLib.buildPackage { src = ./native/password_hashing; };
+
           pname = "sorgenfri";
           version = "0.0.1";
           src = ./.;
@@ -25,13 +38,24 @@
           mixFodDeps = pkgs.beamPackages.fetchMixDeps {
             pname = "${pname}-deps";
             inherit src version;
-            hash = "sha256-b5Ungoq5ft2kemU5K6jbE3cz3iMf1VcNsOHc3qsxqYw=";
+            hash = "sha256-csjrie69NxFBaacpnKkl5AjY7cbe2fHnx0QZ+Xpg91g=";
           };
-          elixir = beam_pkgs.elixir_1_16;
+          elixir = builtins.trace "mixFodDeps: ${mixFodDeps}" beam_pkgs.elixir_1_16;
           sorgenfri = beam_pkgs.mixRelease {
             inherit src pname version mixFodDeps elixir;
 
+            buildInputs = [ pkgs.cargo pkgs.rustc ];
+
             ELIXIR_MAKE_CACHE_DIR = "/tmp/";
+
+            PRECOMPILED_NIF = true;
+
+            RAMBO_PATH = "${inputs.rambo_bin}/bin/rambo";
+
+            preBuild = ''
+              mkdir -p priv/native
+              cp ${passwordHashing}/lib/libpassword_hashing.so priv/native
+            '';
 
             postBuild = ''
               # Consult https://nixos.org/manual/nixpkgs/unstable/#mix-release-example if node dependencies are needed
@@ -86,14 +110,153 @@
             ];
           };
 
+          packages.passwordHashing = passwordHashing;
           packages.sorgenfri = sorgenfri;
           packages.default = self'.packages.sorgenfri;
         };
       flake = {
-        # The usual flake attributes can be defined here, including system-
-        # agnostic ones like nixosModule and system-enumerating ones, although
-        # those are more easily expressed in perSystem.
+        nixosModules.sorgenfri = { options, config, pkgs, lib, ... }:
+          with lib;
+          let
+            name = "sorgenfri";
+            cfg = config.services.sorgenfri;
+            opt = options.services.sorgenfri;
+          in {
+            options.services.sorgenfri = {
+              enable = mkEnableOption "Enables the Sorgenfri service.";
 
+              uploadsDir = mkOption {
+                type = types.path;
+                example = lib.literalExpression "/var/lib/sorgenfri/uploads";
+                default = /var/lib/sorgenfri/uploads;
+                description = "Where to save uploads.";
+              };
+
+              databasePath = mkOption {
+                type = types.path;
+                example = lib.literalExpression "/var/lib/sorgenfri/db.sqlite";
+                default = /var/lib/sorgenfri/db.sqlite;
+                description = "Where to store the SQLite database.";
+              };
+
+              releaseTmp = mkOption {
+                type = types.path;
+                example = lib.literalExpression "/run/leaf";
+                default = /run/leaf;
+                description = ''
+
+                  The value of the RELEASE_TMP environment variable,
+                  which is used to write the state of the VM
+                  configuration when the system is running. It needs to
+                  be a writable directory.
+                '';
+              };
+
+              releaseCookie = mkOption {
+                type = types.str;
+                example = "my_cookie";
+                description =
+                  "The value of the RELEASE_COOKIE environment variable.";
+              };
+
+              address = mkOption {
+                type = types.str;
+                default = "localhost";
+                description = "Address to listen on";
+              };
+
+              port = mkOption {
+                type = types.port;
+                default = 8000;
+                description = "Port to listen on";
+              };
+
+              smtp = {
+                password = mkOption {
+                  type = types.str;
+                  default = "";
+                  description =
+                    "SMTP password. Exactly one of password and passwordFile must be set.";
+                };
+
+                passwordFile = mkOption {
+                  type = types.nullOr types.path;
+                  default = null;
+                  description =
+                    "A file containing the SMTP password. Exactly one of password and passwordFile must be set.";
+                };
+
+                username = mkOption {
+                  type = types.str;
+                  description = "SMTP username";
+                };
+
+                port = mkOption {
+                  type = types.port;
+                  description = "SMTP port";
+                  default = 465;
+                };
+
+                host = mkOption {
+                  type = types.str;
+                  description = "SMTP host";
+                };
+              };
+            };
+
+            config = mkIf cfg.enable {
+              assertions = [{
+                assertion = cfg.smtp.password != opt.smtp.password.default
+                  -> cfg.smtp.passwordFile == null;
+                message =
+                  "Exactly one of password and passwordFile must be set";
+              }];
+
+              systemd.services.sorgenfri = {
+                description = "Sorgenfri Server";
+                wantedBy = [ "multi-user.target" ];
+                after = [ "network.target" ];
+                requires = [ "network-online.target" ];
+
+                startLimitBurst = 3;
+                startLimitIntervalSec = 10;
+
+                environment = {
+                  PHX_SERVER = "true";
+                  PHX_HOST = "${cfg.address}";
+                };
+
+                serviceConfig = {
+                  type = "notify";
+                  DynamicUser = true;
+                  WorkingDirectory = "/run/sorgenfri";
+                  StateDirectory = "sorgenfri";
+                  RuntimeDirectory = "sorgenfri";
+                  # Implied by DynamicUser, but just to emphasize due to RELEASE_TMP
+                  PrivateTmp = true;
+                  ExecStart = ''
+                    ${leaf}/bin/${name} start
+                  '';
+                  ExecReload = ''
+                    ${leaf}/bin/${name} restart
+                  '';
+                  Restart = "on-failure";
+                  RestartSec = 5;
+                  LoadCredential = [
+                    "smtp_password_file:${config.age.secrets.smtp_password.path}"
+                    "secret_key_base:${config.age.secrets.secret_key_base.path}"
+                    "database_url:${config.age.secrets.database_url.path}"
+                  ];
+                  WatchdogSec = "10s";
+                  KillMode = "mixed";
+                };
+                # disksup requires bash
+                path = [ pkgs.bash pkgs.gawk pkgs.typst ];
+
+              };
+
+            };
+          };
       };
     };
 }
--- lib/sorgenfri/password_hashing_nif.ex
+++ lib/sorgenfri/password_hashing_nif.ex
@@ -1,5 +1,29 @@
+defmodule Sorgenfri.PasswordHashingNIF.Macro do
+  defmacro rustler_use() do
+    use_precompiled = System.get_env("PRECOMPILED_NIF", "false")
+
+    cond do
+      use_precompiled == "true" or use_precompiled == "1" ->
+        quote do
+          use Rustler,
+            otp_app: :sorgenfri,
+            skip_compilation?: true,
+            load_from: {:sorgenfri, "priv/native/libpassword_hashing"}
+        end
+
+      true ->
+        quote do
+          use Rustler,
+            otp_app: :sorgenfri,
+            crate: :password_hashing
+        end
+    end
+  end
+end
+
 defmodule Sorgenfri.PasswordHashingNIF do
-  use Rustler, otp_app: :sorgenfri, crate: :password_hashing
+  require Sorgenfri.PasswordHashingNIF.Macro
+  Sorgenfri.PasswordHashingNIF.Macro.rustler_use()
 
   def hash_password(_password), do: error()
   def verify_password(_password, _password_hash, _salt), do: error()
